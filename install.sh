35d41e5d654063573f9053e1b14fc063e3c3d545
Fumio KANEHIRO <f-kanehiro@aist.go.jp>
2015-02-04 22:28:29 +0900

    splits install.sh into getsource.sh and install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,7 @@ source config.sh
 
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 
-sudo apt-get -y install subversion git
-
-cd $SRC_DIR
-git clone https://github.com/fkanehiro/openhrp3.git
-cd openhrp3/util
+cd $SRC_DIR/openhrp3/util
 yes | ./installPackages.sh packages.list.ubuntu.$UBUNTU_VER
 cd ..
 mkdir build
@@ -14,18 +10,13 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCOMPILE_JAVA_STUFF=OFF ..
 make -j2 install
 
-cd $SRC_DIR
-wget https://github.com/OctoMap/octomap/archive/v1.6.8.tar.gz
-tar zxvf v1.6.8.tar.gz
-cd octomap-1.6.8
+cd $SRC_DIR/octomap-1.6.8
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 make -j2 install
 
-cd $SRC_DIR
-yes p | svn co https://atom.a01.aist.go.jp/svn/HRP2/trunk HRP2
-cd HRP2
+cd $SRC_DIR/HRP2
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
@@ -39,9 +30,7 @@ make -j2 install
 # make -j2 install
 # cd ../..
 
-cd $SRC_DIR
-git clone https://github.com/jrl-umi3218/hrp2-drc.git HRP2DRC
-cd HRP2DRC
+cd $SRC_DIR/HRP2DRC
 mkdir build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 make -j2 install
@@ -50,9 +39,7 @@ sudo add-apt-repository -y ppa:v-launchpad-jochen-sprickerhof-de/pcl
 sudo apt-get update
 sudo apt-get -y install libxml2-dev libsdl-dev libglew-dev libcv-dev libcvaux-dev libhighgui-dev libqhull-dev freeglut3-dev libxmu-dev python-dev libboost-python-dev ipython openrtm-aist-python libpcl-all
 
-cd $SRC_DIR
-git clone https://github.com/fkanehiro/hrpsys-base.git
-cd hrpsys-base
+cd $SRC_DIR/hrpsys-base
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCOMPILE_JAVA_STUFF=OFF -DBUILD_KALMAN_FILTER=OFF -DBUILD_STABILIZER=OFF ..
@@ -61,39 +48,21 @@ cd ../..
 
 sudo apt-get -y install libyaml-dev libncurses5-dev
 
-cd $SRC_DIR
-git clone https://github.com/jrl-umi3218/hmc2.git
-cd hmc2
+cd $SRC_DIR/hmc2
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 make -j2 install
 cd ../..
 
-cd $SRC_DIR
-git clone https://github.com/jrl-umi3218/hrpsys-humanoid.git
-cd hrpsys-humanoid
+cd $SRC_DIR/hrpsys-humanoid
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX .. -DCOMPILE_JAVA_STUFF=OFF
 make -j2 install
 cd ../..
 
-#cd $SRC_DIR
-#svn co https://atom.a01.aist.go.jp/svn/hrpeyewalk/trunk hrpeyewalk
-#cd hrpeyewalk
-#mkdir build
-#cd build
-#cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
-#make -j2 install
-#cd ../..
-
-cd $SRC_DIR
-GIT_SSL_NO_VERIFY=1 git clone https://choreonoid.org/git/choreonoid.git
-cd choreonoid/ext
-git clone https://github.com/jrl-umi3218/hrpcnoid.git
-cd ..
-cd misc/script
+cd $SRC_DIR/choreonoid/misc/script
 yes | ./install-requisites-ubuntu-$UBUNTU_VER.sh
 cd ../..
 mkdir build
