8eeaf7ef18607b605153d9206648756a6d4419af
Fumio KANEHIRO <fkanehiro@gmail.com>
2015-08-15 12:37:41 +0900

    adds repositories

--- getsource.sh
+++ getsource.sh
@@ -12,6 +12,7 @@ then
     svn co https://atom.a01.aist.go.jp/svn/HRP2/trunk HRP2
     # svn co https://atom.a01.aist.go.jp/svn/HRP2SH/trunk HRP2SH
     svn co https://atom.a01.aist.go.jp/svn/hrprtc/trunk hrpsys-private
+    git clone https://atom.a01.aist.go.jp/git/HRP2JRVC.git
 fi
 
 git clone https://github.com/jrl-umi3218/hrp2-drc.git HRP2DRC
@@ -21,4 +22,8 @@ git clone https://github.com/jrl-umi3218/hrpsys-humanoid.git
 GIT_SSL_NO_VERIFY=1 git clone https://choreonoid.org/git/choreonoid.git
 cd choreonoid/ext
 git clone https://github.com/jrl-umi3218/hrpcnoid.git
+if [ "$HAVE_ATOM_ACCESS" -eq 1 ]
+then
+    git clone https://atom.a01.aist.go.jp/git/jvrc.git
+fi
 cd ../..
