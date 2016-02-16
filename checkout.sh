76abcf4e96c62b7d3e0d81303e07bdd727bd8775
Fumio KANEHIRO <fkanehiro@gmail.com>
2016-02-17 08:26:05 +0900

    echo directories

--- checkout.sh
+++ checkout.sh
@@ -19,20 +19,25 @@ cd $SRC_DIR
 
 pull_source openhrp3 hrpsys-base hmc2 hrpsys-humanoid
 
+echo HRP2
 cd HRP2
 git pull $REPO
 cd ..
 
+echo HRP2KAI
 cd HRP2KAI
 git pull $REPO
 cd ..
 
+echo hrpsys-private
 cd hrpsys-private
 git pull $REPO
 cd ..
 
+echo choreonoid
 cd choreonoid
 GIT_SSL_NO_VERIFY=1 git pull
+echo choreonoid/ext/hrpcnoid
 cd ext/hrpcnoid
 git pull
 cd ../../..
